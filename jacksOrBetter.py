aaee85e58826b673ba8bcd91af639ddd581a1121
DTooth <43791930+DTooth@users.noreply.github.com>
2022-09-22 18:12:05 -0400

    added most payouts, tested less than most payouts. higher starting credits.

--- jacksOrBetter.py
+++ jacksOrBetter.py
@@ -1,3 +1,4 @@
+from ast import Num
 import os.path
 import os
 from PIL import Image
@@ -10,7 +11,9 @@ pygame.init()
 screen = pygame.display.set_mode((1280, 960))
 pygame.display.set_caption('Jacks or Better')
 clock = pygame.time.Clock()
-casino_font = pygame.font.Font('font/Casino.ttf', 50)
+casino_font = pygame.font.Font('font/casino_font.ttf', 50)
+casino_font_small = pygame.font.Font('font/casino_font.ttf', 35)
+instruction_font = pygame.font.Font('font/raleway.ttf', 20)
 
 # Load Images
 # Start Screen
@@ -19,6 +22,9 @@ start_background_img = pygame.image.load(
 # Background of Game Screen
 table_background_img = pygame.image.load(
     'graphics/table_background.png').convert_alpha()
+table_background_highlight_img = pygame.image.load(
+    'graphics/table_background_highlight.png').convert_alpha()
+table_background_highlight_rect = table_background_highlight_img.get_rect()
 
 # Buttons
 start_button_img = pygame.image.load(
@@ -88,10 +94,19 @@ class Card():
         self.rect.topleft = (0, 0)
         self.selected = False
         self.pressed = False
+    # Card
+
+    def getCardValue(self):
+        return self.value
+
+    def getCardSuit(self):
+        return self.suit
+
+    def getCardColor(self):
+        return self.color
 
     def setPos(self, num):
         # Set card position when drawn
-        print('pos set')
         if num == 0:
             self.rect.topleft = (5, 430)
         elif num == 1:
@@ -147,7 +162,11 @@ class Card():
 
 class Game():
     def __init__(self):
+        # Payout Structure:
+        self.payout = [250, 50, 25, 9, 6, 4, 3, 2, 1]
         # Game Info
+        self.isWin = False
+        self.yellow = (255, 200, 0)
         self.new_game = True
         self.game_active = False
         self.turn_state = 0
@@ -159,6 +178,29 @@ class Game():
         self.curr_deck = []
         # Draw Info
         self.cards_to_draw = 5
+        # Credit / Bet Info
+        self.credit = 100
+        self.bet = 1
+        # Credit / bet Text
+        self.bet_text = casino_font.render(
+            'BET: {}'.format(self.bet), True, self.yellow)
+        self.bet_text_rect = self.bet_text.get_rect()
+        self.bet_text_rect.center = (785, 882)
+
+        self.credit_text = casino_font_small.render(
+            'CREDITS: {}'.format(self.credit), True, (255, 255, 250)
+        )
+        self.credit_text_rect = self.credit_text.get_rect()
+        self.credit_text_rect.topleft = (675, 920)
+
+        self.instruc_text = None
+        self.instruc_text_rect = None
+
+    def setInstrucText(self, text):
+        self.instruc_text = instruction_font.render(
+            text, True, (255, 255, 250))
+        self.instruc_text_rect = self.instruc_text.get_rect()
+        self.instruc_text_rect.topleft = (5, 400)
 
     def getFreshDeck(self):
         unshuffled_deck = []
@@ -173,6 +215,42 @@ class Game():
                 Card(arr[0], arr[1], arr[2], arr[3], pygame.image.load('graphics/cards/{}'.format(i)).convert_alpha(), .5))
         self.curr_deck = unshuffled_deck
         random.shuffle(self.curr_deck)
+
+    def getCredits(self):
+        return self.credit
+
+    def subtractCredits(self, num):
+        if (num > self.credit):
+            return False
+        self.credit = self.credit - num
+        self.credit_text = casino_font_small.render(
+            'CREDITS: {}'.format(self.credit), True, (255, 255, 250)
+        )
+        return True
+
+    def addCredits(self, num):
+        print('{} credits have been added!'.format(num))
+        self.credit = self.credit + num
+        self.credit_text = casino_font_small.render(
+            'CREDITS: {}'.format(self.credit), True, (255, 255, 250)
+        )
+
+    def isWin(self):
+        return self.isWin
+
+    def setIsWin(self, bool):
+        self.isWin = bool
+
+    def getBet(self):
+        return self.bet
+
+    def increaseBet(self):
+        if self.bet == 5:
+            self.bet = 1
+        else:
+            self.bet += 1
+        self.bet_text = casino_font.render(
+            'BET: {}'.format(self.getBet()), True, self.yellow)
 
     def isActive(self):
         return self.game_active
@@ -225,6 +303,111 @@ class Game():
         print(len(self.drawn_cards))
         next_card.setPos(i)
 
+    def checkPayout(self, cards):
+        valueMap = {}
+        suitMap = {}
+        for card in cards:
+            print(card.getCardValue())
+            print(card.getCardSuit())
+            if card.getCardSuit in suitMap.keys():
+                suitMap[card.getCardSuit()] = suitMap[card.getCardSuit()] + 1
+            else:
+                suitMap[card.getCardSuit()] = 1
+
+            if card.getCardValue() in valueMap.keys():
+                valueMap[card.getCardValue()] = valueMap[card.getCardValue()] + 1
+            else:
+                valueMap[card.getCardValue()] = 1
+
+        # TODO: Check payout for given hand
+        payout_level = 0
+        # Check Royal Flush:
+        payout_level += 1
+        # Check Straight Flush:
+        if len(valueMap) == 5:
+            if (len(valueMap.keys()) > 0):
+                print(list(valueMap.keys()))
+                card_values = []
+                for num in list(valueMap.keys()):
+                    card_values.append(int(num))
+                card_values.sort()
+                start = card_values[0]
+                print(card_values)
+                early_break = False
+                for val in card_values:
+                    if not start == val:
+                        early_break = True
+                        break
+                    start += 1
+                if not early_break and len(suitMap) == 1:
+                    print('STRAIGHT - FLUSH !!!!!!!!!!!!!')
+                    return self.bet * self.payout[payout_level]
+
+        payout_level += 1
+        # Check Four of a Kind:
+        if len(valueMap.keys()) == 2:
+            if list(valueMap.values()).sort() == [1, 4]:
+                print('Four of a Kind!!')
+                return self.bet * self.payout[payout_level]
+
+        payout_level += 1
+        # Check Full House
+        if len(valueMap.keys()) == 2:
+            if list(valueMap.values()).sort() == [2, 3]:
+                print('Full House!')
+                return self.bet * self.payout[payout_level]
+
+        payout_level += 1
+        # Check Flush
+        if len(suitMap) == 1:
+            print('FLUSH')
+            return self.bet * self.payout[payout_level]
+        payout_level += 1
+        # Check Straight
+        if len(valueMap) == 5:
+            if (len(valueMap.keys()) > 0):
+                print(list(valueMap.keys()))
+                card_values = []
+                for num in list(valueMap.keys()):
+                    card_values.append(int(num))
+                card_values.sort()
+                start = card_values[0]
+                print(card_values)
+                early_break = False
+                for val in card_values:
+                    if not start == val:
+                        early_break = True
+                        break
+                    start += 1
+                if not early_break:
+                    print("STRAIGHTTTTTTTT")
+                    return self.bet * self.payout[payout_level]
+
+        payout_level += 1
+        # Check Three of a Kind
+        for key in valueMap:
+          # print('key', key, map[key])
+            if valueMap[key] == 3:
+                print('Three of a Kind!')
+                return self.bet * self.payout[payout_level]
+
+        payout_level += 1
+        # Check Two Pair
+        if len(valueMap.keys()) == 3:
+            print('Two Pair!!!')
+            return self.bet * self.payout[payout_level]
+
+        payout_level += 1
+        # Check Jacks or Better
+        for key in valueMap:
+          # print('key', key, map[key])
+            if int(key) >= 11 and valueMap[key] == 2:
+                print('Jacks or Better')
+                return self.bet * self.payout[payout_level]
+        # No Pay
+        return 0
+    # TODO: Return amount to add. bet * payout
+
 
 # Create button instances
 # Start screen buttons
@@ -247,6 +430,7 @@ pygame.time.set_timer(obstacle_timer, 1500)
 # Create Game
 game = Game()
 
+
 # GAME LOOP
 while True:
     # EVENT LOOP
@@ -265,10 +449,29 @@ while True:
     if game.isActive():
         # SCREEN
         screen.blit(table_background_img, (0, 0))
+
         if game.isCardsVisible():
             for card in game.getDrawnCards():
                 card.draw()
-        print(game.getTurnState())
+
+        # print(game.getTurnState())
+        if game.getTurnState() == 0:
+            game.setInstrucText(
+                'Select BET to change bet. Select DRAW to place bet and draw cards.')
+            screen.blit(game.instruc_text, game.instruc_text_rect)
+        if game.getTurnState() == 1:
+            game.setInstrucText(
+                'Select cards to hold. Select DRAW to redraw unselected cards.')
+            screen.blit(game.instruc_text, game.instruc_text_rect)
+        if game.getTurnState() == 2:
+            if game.isWin:
+                game.setInstrucText(
+                    'Congrats you have won! Select DRAW to play again.')
+            else:
+                game.setInstrucText(
+                    'Better luck next time. Selected DRAW to play again.')
+            screen.blit(game.instruc_text, game.instruc_text_rect)
+
         if draw_button.draw():
 
             if game.getTurnState() == 2:
@@ -288,26 +491,40 @@ while True:
                         cards_to_discard.append([card, index])
                     card.unselect()
                     index += 1
-
                 game.setCardsToDraw(len(cards_to_discard))
                 for card in cards_to_discard:
                     game.drawCard(card[1])
+
+                # CHECK PAYOUT FOR CURRENT drawn_cards
+
+                payout = game.checkPayout(game.getDrawnCards())
+                if payout > 0:
+                    game.setIsWin(True)
+                else:
+                    game.setIsWin(False)
+
+                game.addCredits(payout)
+
                 game.setTurnState(2)
 
             else:
                 # TURN JUST STARTED
-                for card in game.getDrawnCards():
-                    card.unselect()
-                game.getFreshDeck()
-                game.setDrawnCards([])
-                game.setCardsToDraw(5)
-                print(game.getCardsToDraw())
-                for i in range(0, game.getCardsToDraw()):
-                    game.drawCard(i)
-                game.setCardsVisible(True)
-                game.setTurnState(1)
+                if game.subtractCredits(game.getBet()):
+                    for card in game.getDrawnCards():
+                        card.unselect()
+                    game.getFreshDeck()
+                    game.setDrawnCards([])
+                    game.setCardsToDraw(5)
+                    print(game.getCardsToDraw())
+                    for i in range(0, game.getCardsToDraw()):
+                        game.drawCard(i)
+                    game.setCardsVisible(True)
 
-        blank_button.draw()
+                    game.setTurnState(1)
+                else:
+                    if game.getCredits() == 0:
+                        print('Out of Credits')
+
         if see_pays_button.draw():
             ...
             # TODO: SEE PAYS SCREEN
@@ -323,6 +540,28 @@ while True:
                 game.setTurnState(0)
                 # TODO: DRAW
 
+        if blank_button.draw():
+            if not game.getTurnState() == 1:
+                game.increaseBet()
+
+        screen.blit(game.bet_text, game.bet_text_rect)
+        # BET UI INFO
+        if game.getBet() == 1:
+            screen.blit(table_background_highlight_img,
+                        (290, 12))
+        elif game.getBet() == 2:
+            screen.blit(table_background_highlight_img,
+                        (480, 12))
+        elif game.getBet() == 3:
+            screen.blit(table_background_highlight_img,
+                        (670, 12))
+        elif game.getBet() == 4:
+            screen.blit(table_background_highlight_img,
+                        (867, 12))
+        else:
+            screen.blit(table_background_highlight_img,
+                        (1065, 12))
+        screen.blit(game.credit_text, game.credit_text_rect)
     else:
         # SCREEN
         screen.blit(start_background_img, (0, 0))
